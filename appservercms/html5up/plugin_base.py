e7ecf9edabbe4855eb8b2865a02e40b2abee610c
Markus Sitzmann <markus.sitzmann@gmail.com>
2016-08-16 00:40:09 +0200

    initial commit

---
+++ appservercms/html5up/plugin_base.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from cmsplugin_cascade.plugin_base import CascadePluginBase
+
+
+from html.parser import HTMLParser
+
+    
+class Html5UpPluginBase(CascadePluginBase):
+    module = 'Html5Up'
+    require_parent = True
+    allow_children = True
+    render_template = 'cascade/generic/wrapper.html'
+
+    @classmethod
+    def unescape_html(cls, html_string):
+        html_parser = HTMLParser()
+        return html_parser.unescape(html_string)
